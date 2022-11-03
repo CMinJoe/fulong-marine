5352ec03077893f343bb280bce07a1893e3392dc
CMinJoe <asd86062377@gmail.com>
2022-11-03 17:46:43 +0800

    First Commit

---
+++ container/page-template/template-home.js
@@ -0,0 +1,48 @@
+export default function TemplateHome({ page, site }) {
+  const { pageHeading } = page || {};
+
+  return (
+    <>
+    <div className="template-home holding-page">
+      <div className="context">
+        <h1>OUR WEBSITE IS CURRENTLY UNDER CONSTRUCTION</h1>
+      </div>
+    </div>
+    <style jsx>{`
+      .template-home {
+        text-align: center;
+      }
+
+      .holding-page{
+        position: fixed;
+        color: var(--cr-black);
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%, -50%);
+      }
+
+      .context {
+        color: #FFFFFF;
+        h1{
+          font-family: 'G0';
+          font-style: normal;
+          font-weight: 400;
+          font-size: 24px;
+          line-height: 34px;
+          text-transform: uppercase;
+        }
+
+        h2{
+          margin-top:15px;
+          font-family: 'G0';
+          font-style: normal;
+          font-weight: 400;
+          font-size: 24px;
+          line-height: 34px;
+        }
+        
+      }
+    `}</style>
+  </>
+  );
+}
